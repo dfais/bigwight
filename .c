6da6035409dd4ae11ad811bff2f0b0f2db5aa40c
dfais <78713905+dfais@users.noreply.github.com>
2021-02-08 12:10:36 +0800

    c
    c

---
+++ .c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+	struct Student
+	{
+		int num;
+		char name[20];
+		float score;
+	}student1,student2;
+	scanf("%d%s%f",&student1.num,student1.name,&student1.score);
+    scanf("%d%s%f",&student2.num,student2.name,&student2.score);
+	printf("The higher score is:\n");
+	if(student1.score>student2.score)
+		printf("%d   %s   %4.2f\n",student1.num, student1.name, student1.score);
+	else
+		printf("%d   %s   %4.2f\n",student2.num, student2.name, student2.score);
+}
